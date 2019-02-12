e02ad152a1bc5d58bfdbcb84a69d6e1fab60dc0e
Tang Yang <ty4081097@gmail.com>
2019-02-11 18:57:03 -0600

    add updat userProfile page.

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -19,34 +19,10 @@ class UserProfile extends Component {
 
         const action = {type: 'GET_FAMILY' };
         this.props.dispatch(action);
-    //    axios({
-    //        method: 'GET',
-    //        url: '/api/template'
-    //     }).then((response) =>{
-    //        console.log(response.data);
-    //        this.setState({
-    //         familyList: response.data
-    //        });
-    //     }).catch((error)=> {
-    //         console.log('this is error in getFamily');
-    //     })
-        
     }
 
-    // mapFamily = () => {
-    //     return(
-    //         this.props.reduxStore.familyReducer.map((member, i) => {
-    //             return (
-    //                 <li key={i}>{member.first_name} {member.first_last} {member.date_of_birth}
-    //                     {member.gender} {member.description} {member.image_url}</li>
-    //             )
-    //         })
-    //     )
-    // }
-
   
-
-    render(){
+        render(){
         
         return(
             <div>
