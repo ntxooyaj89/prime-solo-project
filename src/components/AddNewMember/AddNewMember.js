6087eec1294fba515735c69ca2e7349178bc8f4f
Tang Yang <ty4081097@gmail.com>
2019-02-13 14:41:44 -0600

    add family name in drop down.

--- src/components/AddNewMember/AddNewMember.js
+++ src/components/AddNewMember/AddNewMember.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
+import { TablePagination } from '@material-ui/core';
 
 class AddNewMember extends Component {
 
@@ -25,16 +26,16 @@ class AddNewMember extends Component {
         // this.getFamily();
         // this get just the name of family
         this.getFamilyName();
-        
-    }
-    
-    // this gets the family name
-    getFamilyName = () =>{
-       const action = {type: 'GET_FAMILY_NAME'};
-       this.props.dispatch(action);
+
     }
 
-   // this gets the family members
+    // this gets the family name
+    getFamilyName = () => {
+        const action = { type: 'GET_FAMILY_NAME' };
+        this.props.dispatch(action);
+    }
+
+    // this gets the family members
     // getFamily = () => {
 
     //     const action = { type: 'GET_FAMILY', payload: { id: this.props.match.params.id }  };
@@ -50,33 +51,33 @@ class AddNewMember extends Component {
     }
 
     render() {
-        
+
         return (
-           
-               <div>
-                  
+
+            <div>
+
                 {JSON.stringify(this.props.reduxStore.familyReducer.myFamilyName)}
-               
+
                 <form onSubmit={this.addNewMember}>
-               
-                    <input type='text' placeholder="first name" onChange={this.handleFirstName}/>
-                    <input type='text' placeholder="last name" onChange={this.handleLastName}/>
-                    <input type='text' placeholder="date of birth" onChange={this.handleBirthday}/>
-                    <input type='text' placeholder="gender" onChange={this.handleGender}/>
-                    <input type='text' placeholder="description" onChange={this.handleDescribtion}/>
-                    <input type='text' placeholder="image" onChange={this.handleImage}/>
+
+                    <input type='text' placeholder="first name" onChange={this.handleFirstName} />
+                    <input type='text' placeholder="last name" onChange={this.handleLastName} />
+                    <input type='text' placeholder="date of birth" onChange={this.handleBirthday} />
+                    <input type='text' placeholder="gender" onChange={this.handleGender} />
+                    <input type='text' placeholder="description" onChange={this.handleDescribtion} />
+                    <input type='text' placeholder="image" onChange={this.handleImage} />
                     <select onChange={this.selectFamilyName} >
-                    <option >Slecte family name</option>
-                  
-                    
-                    
+                        <option >Slect family name</option>
+                        {this.props.reduxStore.familyReducer.myFamilyName.map((family, i) => {
+                            return <option key={i} value={family.id}>{family.family_name}</option>
+                        })}
                     </select>
-                   
+
 
                 </form>
             </div>
 
-          
+
         )
     }
 }
@@ -85,4 +86,4 @@ const mapReduxStoreToProps = (reduxStore) => ({
     reduxStore
 });
 
-export default connect(mapReduxStoreToProps) (AddNewMember);
+export default connect(mapReduxStoreToProps)(AddNewMember);
