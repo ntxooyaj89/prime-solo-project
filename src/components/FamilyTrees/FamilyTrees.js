1df697397b541b136cacdfe0855a6107af87e853
Tang Yang <ty4081097@gmail.com>
2019-02-15 13:15:50 -0600

    add a history.push to send to another component.

--- src/components/FamilyTrees/FamilyTrees.js
+++ src/components/FamilyTrees/FamilyTrees.js
@@ -6,7 +6,7 @@ class FamilyTree extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            familyTree: []
+            person: '',
         }
     }
 
@@ -14,6 +14,7 @@ class FamilyTree extends Component {
         this.getUserFamily();
     }
 
+    // get the family names of the user
     getUserFamily = () => {
         // const userId = this.props.reduxStore.user.id
         const action = { type: 'GET_USER_FAMILY' };
@@ -21,32 +22,39 @@ class FamilyTree extends Component {
 
     }
 
+    handleUserFamily = (event) => {
+        console.log('this is handleUserFamily');
+        this.setState({
+            person: event.target.value,
+        });
+        this.props.history.push(`/family-profile/${event.target.value}`);
+    }
+
+    
+
 
     render() {
 
         return(
+           
+           <div>
+               {JSON.stringify(this.props.reduxStore.familyReducer.myFamilyName)}
+           <ul>
+                    {this.props.reduxStore.familyReducer.myFamilyName.map((user, i) => {
+                        return (
+                            // set the value to the id of the object and use it to send to next component.
+                        <button onClick={this.handleUserFamily} key={i} value={user.id}>{user.family_name}</button>
+                        )
 
-            <div>
-               
+                    })}
 
-                <form onSubmit={this.handleUserFamily}>
-                    <select onChange={this.selectFamily} placeholder="select a family" >
-                        <option value="">select a Family</option>
-                        {this.props.reduxStore.familyReducer.myFamilyName.map((user, i) => {
-                            return <option key={i} value={user.id}>{user.family_name}</option>
+           </ul>
 
-                        })}
-                                      
-                </select>
-
-                <button type="submit">submit</button>
-
-                </form>
-
-                
+            
                                         
                 
             </div>
+            
             
                
         );
@@ -59,9 +67,18 @@ class FamilyTree extends Component {
 }
                 
 const mapReduxStoreToProps = (reduxStore) => ({
-                        reduxStore
-                    });
-                    
-                    export default connect(mapReduxStoreToProps)(FamilyTree);
+    reduxStore
+});
+
+
+
+export default connect(mapReduxStoreToProps)(FamilyTree);
                     
 // payload: {id: this.props.reduxStore.user.id }
+
+// {
+//     this.props.reduxStore.familyReducer.myFamilyName.map((user, i) => {
+//         return <option key={i} value={user.id}>{user.family_name}</option>
+
+//     })
+// }
