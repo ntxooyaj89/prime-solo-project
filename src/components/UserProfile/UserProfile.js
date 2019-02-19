50989122a88c3bb7ae2bd652eee10270e4d087be
Tang Yang <ty4081097@gmail.com>
2019-02-19 08:39:48 -0600

    add card to user-profile.

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -4,55 +4,114 @@ import Card from '@material-ui/core/Card';
 import { CardActionArea, CardMedia, CardContent, Typography, CardActions } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import moment from 'moment';
+import './UserProfile.css';
 
 
-class UserProfile extends Component{
+class UserProfile extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state ={}
+        this.state = {
+            add: true,
+            card: { maxWidth: '150', },
+            media: { height: '150', paddingTop: '56.25%' },
+            actions: { display: 'flex' },
+            expand: { transform: 'rotate(0deg)', marginLeft: 'auto' },
+            expandOpen: { transform: 'rotate(180deg)' }
+        };
     }
 
     componentDidMount = () => {
-       console.log('this has mounted in user profile');
-       this.getMemberDetail();
+        console.log('this has mounted in user profile');
+        this.getMemberDetail();
     }
 
 
     getMemberDetail = () => {
         console.log('this is inside getMemberDetail');
         const memberId = this.props.match.params.id;
-        const action = {type: 'GET_MEMBER_DETAIL', payload: {memberId: memberId}}
+        const action = { type: 'GET_MEMBER_DETAIL', payload: { memberId: memberId } }
         this.props.dispatch(action);
     }
 
-   
-    render(){
-        return(
+
+    render() {
+        return (
             <div>
-                <div>User Profile</div>
+
                 {/* {JSON.stringify(this.props.match.params)} */}
-                {JSON.stringify(this.props.reduxStore.familyReducer.memberDetail)}
+                {/* {JSON.stringify(this.props.reduxStore.familyReducer.memberDetail)} */}
                 {this.props.reduxStore.familyReducer.memberDetail.map(person => {
-                    return(
-                    <div key={person.id}>
-                    <img src={person.image}/>
-                    {person.first_name}
-                    {person.last_name}
-                    {person.date_of_birth}
-                    {person.gender}
-                    {person.description}
-                    {JSON.stringify(person.have_we_met)}
+                    return (
+                        <div className="card" key={person.id}>
+                            <Card className={this.state.card}>
+                                <CardActionArea>
+                                    <CardMedia
+                                        component="img"
+                                        className={this.state.media}
+                                        alt="person of family"
+                                        height="250"
+                                        image={person.image}
+                                    />
+                                    <CardContent>
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        {person.first_name}
+                                    </Typography>
+
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        {person.last_name}
+                                    </Typography>
+
+                                    <Typography>
+                                        Date of Birth: {moment(person.date_of_birth).format("MMMM D YYYY")}
+
+                                    </Typography>
+
+                                    <Typography>
+                                        {person.gender}
+
+                                    </Typography>
+
+                                    <Typography>
+                                        {person.description}
+
+                                    </Typography>
+
+                                    <Typography>
+                                        {JSON.stringify(person.have_we_met)}
+
+                                    </Typography>
+
+                                   </CardContent>
+
+                                </CardActionArea>
+                                <CardActions>
+
+
+                                    <Button size="small" color="primary" onClick={this.handelDelete}>Delete Member</Button>
+                                    <Button size="small" color="primary" onClick={this.handelUpdate}>We've met</Button>
+
+
+                                </CardActions>
+
+
+                            </Card>
+                        </div>
+                       
+                        
 
 
 
-                    </div>
+
+
 
                     )
+                   
                 })}
-
             </div>
-           
+
+
+
 
         )
     }
@@ -62,4 +121,4 @@ const mapStateToProps = reduxStore => ({
     reduxStore: reduxStore
 })
 
-export default connect(mapStateToProps) (UserProfile);
+export default connect(mapStateToProps)(UserProfile);
